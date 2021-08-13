f36d4d4d25c8e03533fe67d4208dc52d2782a59a
juliane1999 <lastjuliane@gmail.com>
2021-08-12 20:35:09 -0700

    completed project using context api

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
-//start
+import ProductContext from './contexts/ProductContext';
+import CartContext from './contexts/CartContext';
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+
 
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
-		// add the given item to the cart
+		setCart([...cart,item])
 	};
 
 	return (
+		<ProductContext.Provider value={{ products, addItem }} >
+			<CartContext.Provider value = {cart}>
 		<div className="App">
 			<Navigation cart={cart} />
 
 			{/* Routes */}
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
+				<Products />
 			</Route>
 
 			<Route path="/cart">
 				<ShoppingCart cart={cart} />
 			</Route>
 		</div>
+		</CartContext.Provider>
+		</ProductContext.Provider>
 	);
 }
 
